58a37beeb35f7d9585323c8feefd7cb066ca9a6c
David Fernandez <david_ferm@hotmail.com>
2018-12-04 06:25:47 +0100

    Update MAP.js

--- js/MAP.js
+++ js/MAP.js
@@ -79,7 +79,10 @@ export class Mapa {
             .then(function(res) {
                 let respuesta = res.json();
                 return respuesta;
-            });
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
 
         let baseDatosMets = data.parkimetros;
 
